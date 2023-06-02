81874109da9976a11cf2c7543f58ef870da63a0d
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-02 12:39:38 +0200

    init: contact page

---
+++ web/app/(main)/contact/page.tsx
@@ -0,0 +1,18 @@
+import { Heading } from '@/components/heading/heading';
+
+export const metadata = {
+	title: 'Contact with Library',
+	openGraph: {
+		title: 'Contact with Library',
+	},
+};
+
+const ContactPage = () => {
+	return (
+		<Heading tag="h1" size="large">
+			Contact with Library
+		</Heading>
+	);
+};
+
+export default ContactPage;
