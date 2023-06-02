c7048396e983326caffafb84f1fb9f28d072cc15
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-02 12:48:38 +0200

    add: description field

--- migrations/init_db.py
+++ migrations/init_db.py
@@ -47,9 +47,9 @@ try:
 
     print(yellow("Inserting books..."))
     cur.execute(
-        "INSERT INTO book (author_id, publisher_id, title, publication_date, genre, is_available, number_of_pages, "
+        "INSERT INTO book (author_id, publisher_id, title, description, publication_date, genre, is_available, number_of_pages, "
         "cover_image, ISBN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        (1, 1, 'Harry Potter i Kamień Filozoficzny', '1997-06-26', 'Fantastyka', 'True', 336, 'harry_potter.jpg',
+        (1, 1, 'Harry Potter i Kamień Filozoficzny', '"Harry Potter i Kamień Filozoficzny" to pierwsza część niezwykle popularnej serii książek napisanych przez J.K. Rowling. Opowieść rozpoczyna się, gdy sierota o imieniu Harry Potter dowiaduje się, że jest czarodziejem i zostaje przyjęty do Hogwartu, szkoły magii i czarodziejstwa. Wraz z przyjaciółmi Hermioną Granger i Ronem Weasleyem, Harry wchodzi w świat pełen tajemnic, magii i niebezpieczeństw. Głównym wątkiem książki jest poszukiwanie tajemniczego Kamienia Filozoficznego, który daje niezwykłą moc swojemu posiadaczowi. Czy Harry i jego przyjaciele zdołają odkryć tajemnice, które kryje szkoła i pokonać złego Lorda Voldemorta? "Harry Potter i Kamień Filozoficzny" to fascynująca i pełna przygód historia, która wprowadza czytelników w magiczny świat Hogwartu.', '1997-06-26', 'Fantastyka', 'True', 336, 'harry_potter.jpg',
          '9788700631625'))
     cur.execute(
         "INSERT INTO book (author_id, publisher_id, title, publication_date, genre, is_available, number_of_pages, "
