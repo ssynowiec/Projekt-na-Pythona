8adbe6361f57b3b64d3530b6c63ae06ee90d22a1
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-07 20:06:38 +0200

    add: primary data in your-profile

--- web/app/(dashboard)/your-profile/page.tsx
+++ web/app/(dashboard)/your-profile/page.tsx
@@ -1,11 +1,31 @@
-import { Container } from '@/components/container/container';
+'use client'
+
+import {Container} from '@/components/container/container';
+import {Heading} from "@/components/heading/heading";
+import {Input} from "@/components/input/input";
+import {useSession} from "next-auth/react";
 
 const DashboardPage = () => {
-	return (
-		<Container>
-			<h1>Dashboard</h1>
-		</Container>
-	);
+    const {data, status} = useSession();
+
+    return (
+        <Container>
+            <div className='flex'>
+                <section>Nav</section>
+                <section><Heading tag='h1' size='large'>Your Profile</Heading>
+                    <form>
+                        <div className='flex flex-col md:flex-row'>
+                            <Input label='Your name' id='name' name='name' type='text' required={true} disabled={true}
+                                   value={data?.user?.name}/>
+                            <Input label='Your surname' id='surname' name='surname' type='text' required={true}
+                                   disabled={true}
+                                   value={data?.user?.surname}/>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </Container>
+    );
 };
 
 export default DashboardPage;
--- web/components/input/input.tsx
+++ web/components/input/input.tsx
@@ -1,65 +1,72 @@
-import { ChangeEvent, forwardRef } from 'react';
+import {ChangeEvent, forwardRef} from 'react';
 import clsx from 'clsx';
 
 type InputProps = {
-	label: string;
-	id: string;
-	name: string;
-	type: string;
-	autoComplete?: string;
-	required: boolean;
-	placeholder?: string;
-	error?: string;
-	onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    label: string;
+    id: string;
+    name: string;
+    type: string;
+    autoComplete?: string;
+    required: boolean;
+    placeholder?: string;
+    error?: string;
+    disabled?: boolean;
+    value?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-	(
-		{
-			label,
-			id,
-			name,
-			type = 'text',
-			autoComplete,
-			required,
-			placeholder,
-			error,
-			onChange,
-			...rest
-		},
-		ref,
-	) => {
-		return (
-			<div>
-				<label
-					htmlFor={id}
-					className="block text-sm font-medium leading-6 text-gray-900"
-				>
-					{label}
-				</label>
-				<div className="mt-2">
-					<input
-						ref={ref}
-						id={id}
-						type={type}
-						autoComplete={autoComplete}
-						required={required}
-						placeholder={placeholder}
-						onChange={onChange}
-						name={name}
-						{...rest}
-						className={clsx(
-							'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-							error && 'ring-2 ring-red-500',
-						)}
-					/>
-				</div>
-				{error && (
-					<p className="mt-1 text-sm text-red-600" id="email-error">
-						{error}
-					</p>
-				)}
-			</div>
-		);
-	},
+    (
+        {
+            label,
+            id,
+            name,
+            type = 'text',
+            autoComplete,
+            required,
+            placeholder,
+            error,
+            disabled = false,
+            value,
+            onChange,
+            ...rest
+        },
+        ref,
+    ) => {
+        return (
+            <div>
+                <label
+                    htmlFor={id}
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                    {label}
+                </label>
+                <div className="mt-2">
+                    <input
+                        ref={ref}
+                        id={id}
+                        type={type}
+                        autoComplete={autoComplete}
+                        required={required}
+                        placeholder={placeholder}
+                        onChange={onChange}
+                        name={name}
+                        disabled={disabled}
+                        value={value}
+                        {...rest}
+                        className={clsx(
+                            'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                            error && 'ring-2 ring-red-500',
+                            disabled && 'bg-gray-100',
+                        )}
+                    />
+                </div>
+                {error && (
+                    <p className="mt-1 text-sm text-red-600" id="email-error">
+                        {error}
+                    </p>
+                )}
+            </div>
+        );
+    },
 );
