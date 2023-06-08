e8d1877348765b634e2832158818627bfb60d4d9
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 18:40:13 +0200

    add: role field

--- migrations/schema/login_data.schema.sql
+++ migrations/schema/login_data.schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE login_data (
     login VARCHAR(50) NOT NULL PRIMARY KEY,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
