603c2088c4c3eab6a0f12108832bd1bf0a5dd0c8
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 13:51:46 +0200

    init: change password page

---
+++ web/app/(dashboard)/change-password/page.tsx
@@ -0,0 +1,7 @@
+import {Heading} from "@/components/heading/heading";
+
+const ChangePasswordPage = () => {
+    return <Heading tag='h1' size='large'>Change password</Heading>
+}
+
+export default ChangePasswordPage
