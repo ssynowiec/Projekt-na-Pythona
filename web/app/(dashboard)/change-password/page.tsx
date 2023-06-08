9bdec6e75a745508cef69d73c1ba53dfe32fbe33
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 13:58:50 +0200

    add: basic change password form

--- web/app/(dashboard)/change-password/page.tsx
+++ web/app/(dashboard)/change-password/page.tsx
@@ -1,7 +1,26 @@
 import {Heading} from "@/components/heading/heading";
+import {Input} from "@/components/input/input";
+import {Button} from "@/components/button/button";
 
 const ChangePasswordPage = () => {
-    return <Heading tag='h1' size='large'>Change password</Heading>
+    return <>
+        <Heading tag='h1' size='large'>Change password</Heading>
+        <form>
+
+            <Input label='Current password' id='current_password' name='current_password' type='password'
+                   placeholder="***********"
+                   required={true}/>
+            <Input label='New password' id='new_password' name='new_password'
+                   placeholder="***********"
+                   type='password'
+                   required={true}/>
+            <Input label='Repeat new password' id='repeat_new_password'
+                   placeholder="***********"
+                   name='repeat_new_password' type='password'
+                   required={true}/>
+            <Button type='submit'>Update password</Button>
+        </form>
+    </>
 }
 
 export default ChangePasswordPage
