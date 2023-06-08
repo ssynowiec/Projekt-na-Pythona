69a15fbcacaaac406ddd6dc1c5cff31446582cf1
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 18:32:33 +0200

    add: default pages

---
+++ web/app/(dashboard)/my-rentals/page.tsx
@@ -0,0 +1,14 @@
+import {Heading} from "@/components/heading/heading";
+
+export const metadata = {
+    title: 'My rentals',
+    openGraph: {
+        title: 'My rentals',
+    },
+};
+
+const MyRentalsPage = () => {
+    return <Heading tag='h1' size='large'>Coming soon</Heading>
+}
+
+export default MyRentalsPage;
---
+++ web/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,14 @@
+import {Heading} from "@/components/heading/heading";
+
+export const metadata = {
+    title: 'Settings',
+    openGraph: {
+        title: 'Settings',
+    },
+};
+
+const SettingsPage = () => {
+    return <Heading tag='h1' size='large'>Coming soon</Heading>
+}
+
+export default SettingsPage;
