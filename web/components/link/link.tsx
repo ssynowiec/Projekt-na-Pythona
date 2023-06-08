9d6708a83c0a7b9be4dcc20ec24fc74aed5574de
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 13:39:07 +0200

    add: classNames to link component

--- web/components/link/link.tsx
+++ web/components/link/link.tsx
@@ -1,29 +1,31 @@
 import NextLink from 'next/link';
-import type { ReactNode } from 'react';
+import type {ReactNode} from 'react';
+import clsx from "clsx";
 
 type LinkProps = {
-	type?: 'inline' | 'button';
-	href: string;
-	children: ReactNode;
+    type?: 'inline' | 'button';
+    href: string;
+    className?: string;
+    children: ReactNode;
 };
 
-export const Link = ({ type = 'inline', href, children }: LinkProps) => {
-	if (type === 'inline')
-		return (
-			<NextLink
-				href={href}
-				className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-			>
-				{children}
-			</NextLink>
-		);
-	else
-		return (
-			<a
-				href={href}
-				className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-			>
-				{children}
-			</a>
-		);
+export const Link = ({type = 'inline', href, className, children}: LinkProps) => {
+    if (type === 'inline')
+        return (
+            <NextLink
+                href={href}
+                className={clsx("font-semibold leading-6 text-indigo-600 hover:text-indigo-500", className)}
+            >
+                {children}
+            </NextLink>
+        );
+    else
+        return (
+            <a
+                href={href}
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+                {children}
+            </a>
+        );
 };
