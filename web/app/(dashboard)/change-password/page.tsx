a20083c1ba4957fcab0fab71dae3996e8d8f483e
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 14:03:29 +0200

    add: mark as required

--- web/app/(dashboard)/change-password/page.tsx
+++ web/app/(dashboard)/change-password/page.tsx
@@ -9,17 +9,21 @@ const ChangePasswordPage = () => {
 
             <Input label='Current password' id='current_password' name='current_password' type='password'
                    placeholder="***********"
+                   markAsRequired={true}
                    required={true}/>
             <Input label='New password' id='new_password' name='new_password'
                    placeholder="***********"
+                   markAsRequired={true}
                    type='password'
                    required={true}/>
             <Input label='Repeat new password' id='repeat_new_password'
                    placeholder="***********"
+                   markAsRequired={true}
                    name='repeat_new_password' type='password'
                    required={true}/>
             <Button type='submit'>Update password</Button>
         </form>
+        <p><span className="text-red-500">*</span> - This field is required</p>
     </>
 }
 
