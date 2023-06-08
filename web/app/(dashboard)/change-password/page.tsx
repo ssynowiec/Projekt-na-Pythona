c13bd606828f09baf737d9eed3d670dcb890dc94
Stanisław Synowiec <stasyn1410@gmail.com>
2023-06-08 14:05:44 +0200

    add: meta title

--- web/app/(dashboard)/change-password/page.tsx
+++ web/app/(dashboard)/change-password/page.tsx
@@ -2,6 +2,13 @@ import {Heading} from "@/components/heading/heading";
 import {Input} from "@/components/input/input";
 import {Button} from "@/components/button/button";
 
+export const metadata = {
+    title: 'Change your password',
+    openGraph: {
+        title: 'Change your password',
+    },
+};
+
 const ChangePasswordPage = () => {
     return <>
         <Heading tag='h1' size='large'>Change password</Heading>
